8658277dbe5b2df54e58a42c0a4e21b085469342
AwrenNuit <a.rennaker@yahoo.com>
2019-11-26 10:12:02 -0600

    added server POST

--- server/routes/router.js
+++ server/routes/router.js
@@ -13,6 +13,19 @@ router.get(`/`, (req, res)=>{
     })
 })
 
+router.post(`/`, (req, res)=>{
+    console.log('in / POST');
+    let id = [req.body.cost, req.body.sqft, req.body.type, req.body.city, req.body.image_path];
+    let query = `INSERT INTO listings (cost, sqft, type, city, image_path) VALUES($1, $2, $3, $4, $5);`;
+    pool.query(query, id)
+    .then(result=>{
+        res.sendStatus(201);
+    }).catch(error=>{
+        console.log('ERROR POSTING A LISTING ------------------------>', error);
+        res.sendStatus(500);
+    })
+})
+
 
 
 module.exports = router;
