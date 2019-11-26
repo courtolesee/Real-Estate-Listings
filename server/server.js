f96fe8caf90d59e999b7e18381e4ad6aa66b3245
AwrenNuit <a.rennaker@yahoo.com>
2019-11-26 09:41:51 -0600

    tessssttttt

---
+++ server/server.js
@@ -0,0 +1,40 @@
+// to start:
+// npm init --yes
+// git init
+// npm install express pg
+// make .gitignore: (node_modules, .DS_Server)
+// copy and paste this in package.json scripts: "start": "node server/server.js"
+
+// requires
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
+const pg = require( 'pg' );
+
+// uses
+app.use( express.static( 'server/public' ) ) ;
+app.use( bodyParser.urlencoded( { extended: true } ) );
+
+// db stuff
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: '',
+    host: '',
+    port: 5432,
+    max: 12,
+    idleTimeoutMillis: 30000
+}); // end pool
+
+pool.on('connect', ()=>{
+    console.log( 'connected to db' );
+})
+
+// globals
+const port = 5001;
+
+// server up
+app.listen( port, ()=>{
+    console.log( 'server up on:', port );
+}) //end server up  
+
+// routes
